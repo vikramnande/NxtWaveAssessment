23d698a9cfb147469a64fdb4484d32fb5d1ad919
Vikram Nande <vikramnande@gmail.com>
2022-12-24 19:49:23 +0530

    first commit

---
+++ src/main/java/com/vikram/nxtwave/Controller/TempController.java
@@ -0,0 +1,25 @@
+package com.vikram.nxtwave.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class TempController {
+
+    @ApiIgnore
+    @RequestMapping(value="/")
+    public void redirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
+    }
+
+    @GetMapping (value = "/temp")
+    public String temp1() {
+        return "true";
+    }
+}
---
+++ src/test/java/com/vikram/nxtwave/NxtwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikram.nxtwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NxtwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
